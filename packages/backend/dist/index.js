943f3f75f0cfbda692e75d9eabc1ba84425c5f6a
kkito0726 <k.kito0726@gmail.com>
2023-02-24 18:33:42 +0900

    ミニマムなApollo Serverの立ち上げ

--- packages/backend/dist/index.js
+++ packages/backend/dist/index.js
@@ -1,2 +1,43 @@
-console.log("hello from index");
-export {};
+import http from "http";
+import { ApolloServer } from "@apollo/server";
+import { ApolloServerPluginDrainHttpServer } from "@apollo/server/plugin/drainHttpServer";
+import { expressMiddleware } from "@apollo/server/express4";
+import express from "express";
+import { mergeResolvers, mergeTypeDefs } from "@graphql-tools/merge";
+const typeDefs = `#graphql
+ type Query{
+  health: Boolean
+ }
+`;
+const resolvers = {
+    Query: {
+        // eslint-disable-next-line @typescript-eslint/require-await
+        health: async (_, args, context, info) => {
+            return true;
+        },
+    },
+};
+async function main() {
+    const PORT = process.env.PORT || 5555;
+    const app = express();
+    const httpServer = http.createServer(app);
+    const server = new ApolloServer({
+        typeDefs: mergeTypeDefs([typeDefs]),
+        resolvers: mergeResolvers([resolvers]),
+        plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+    });
+    await server.start();
+    app.use(express.json());
+    app.use("/graphql", expressMiddleware(server, {
+        // eslint-disable-next-line @typescript-eslint/require-await
+        context: async ({ req, res }) => ({ req, res }),
+    }));
+    await new Promise((resolve) => {
+        httpServer.listen({ port: PORT }, resolve);
+    });
+    console.log(`🚀 server is up and running at http://localhost:${PORT}`);
+}
+main().catch((err) => {
+    console.error(err);
+    process.exit(1);
+});
