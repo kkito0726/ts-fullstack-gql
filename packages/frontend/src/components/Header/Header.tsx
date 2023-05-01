f114b8f3b4bb4192e86946b278bd545ec86a438f
kkito0726 <k.kito0726@gmail.com>
2023-05-02 08:58:34 +0900

    ダークモード切り替えボタンの追加

--- packages/frontend/src/components/Header/Header.tsx
+++ packages/frontend/src/components/Header/Header.tsx
@@ -1,8 +1,10 @@
 import React from "react";
+import { BsMoon, BsSun } from "react-icons/bs";
 
 type HeaderProps = {};
 
 export const Header: React.FC<HeaderProps> = ({}) => {
+  const isDarkMode = false;
   return (
     <header className="px-6 py-5 border-b bg-white">
       <div className="flex justify-between">
@@ -11,7 +13,9 @@ export const Header: React.FC<HeaderProps> = ({}) => {
         </section>
         <section>
           <ul>
-            <li>Item 1</li>
+            <li>
+              <button>{isDarkMode ? <BsSun /> : <BsMoon />}</button>
+            </li>
           </ul>
         </section>
       </div>
