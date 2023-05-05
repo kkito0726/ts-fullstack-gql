9a48737d40cf3fc46b231c30af563b7e7d23bd4f
kkito0726 <k.kito0726@gmail.com>
2023-05-05 23:38:20 +0900

    Headerを固定して少し細くした

--- packages/frontend/src/components/Header/Header.tsx
+++ packages/frontend/src/components/Header/Header.tsx
@@ -7,7 +7,7 @@ type HeaderProps = {};
 export const Header: React.FC<HeaderProps> = ({}) => {
   const { isDarkMode, toggleIsDarkMode } = useDarkModeContext();
   return (
-    <header className="px-6 py-5 border-b bg-white dark:bg-zinc-800 dark:border-zinc-700">
+    <header className="px-6 py-3 border-b bg-white sticky top-0 dark:bg-zinc-800 dark:border-zinc-700">
       <div className="flex justify-between">
         <section>
           <span className="text-slate-600 font font-semibold dark:text-zinc-100">
